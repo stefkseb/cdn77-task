a657d6bd0c0eea0e2f9041a0ad3ec686125aabf5
Sebastian Stefko <sebastianstefko@gmail.com>
2024-03-16 16:58:04 +0100

    Fixed nginx rproxy, created installDaemon role

--- roles/installDaemon/files/statusChecker.py
+++ roles/installDaemon/files/statusChecker.py
@@ -2,8 +2,7 @@ import requests
 import json
 
 payload = {'query': 'node_load1'}
-# res = requests.get('http://127.0.0.1:9090/api/v1/query', params=payload)
-res = requests.get('http://192.168.50.145:9090/api/v1/query', params=payload)
+res = requests.get('http://127.0.0.1:9090/api/v1/query', params=payload)
 
 data_to_write = {
         "monitoring": [],
@@ -14,4 +13,5 @@ data_json = json.loads(res.text)
 for rec in data_json["data"]["result"]:
         data_to_write["monitoring"].append({rec["metric"]["instance"]: float(rec["value"][1])})
 
-print(json.dumps(data_to_write, indent=4))
+with open("/var/log/stats", "w") as f:
+        f.write(json.dumps(data_to_write, indent=4))
